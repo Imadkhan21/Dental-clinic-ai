38549f3acc44a58d2bc59b04843853c3d6209c43
Imad Khan <21pwdsc0015@uetpeshawar.edu.pk>
2025-08-19 13:46:44 +0500

    Commit all visual changes

--- app.py
+++ app.py
@@ -407,5 +407,5 @@ def clear_chat():
 
 # === Entry Point ===
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5005))
+    port = int(os.environ.get('PORT', 5004))
     app.run(host='0.0.0.0', port=port, debug=True)
